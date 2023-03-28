6514c647a81b1793fbd57dd2f157d601fa6bf63d
Romain Giovanetti <ganoninc@gmail.com>
2023-03-28 21:39:29 +1100

    add convert() and getLetters()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const ZERO = "zéro";
 const UN = "un";
@@ -29,271 +29,284 @@ const MILLIARD = "milliard";
 const MOINS = "moins";
 
 const tab = [
-    "",
-    MILLE,
-    MILLION,
-    MILLIARD,
-    MILLE + " " + MILLIARD,
-    MILLION + " de " + MILLIARD,
-    MILLIARD + " de " + MILLIARD 
+  "",
+  MILLE,
+  MILLION,
+  MILLIARD,
+  MILLE + " " + MILLIARD,
+  MILLION + " de " + MILLIARD,
+  MILLIARD + " de " + MILLIARD,
 ];
 
-function getDigitInWrittenForm(digitAsNumber)
-{
-    let digitInWrittenForm = null;
-    if(digitAsNumber === null || digitAsNumber === undefined || isNaN(digitAsNumber)){
-        throw new Error("The passed argument is not a number");
-    }else if ((digitAsNumber < 0) || (digitAsNumber > 9)){
-        throw new Error("The pasased digit must be any one of the ten numbers 0 to 9");
-    }
-    
-    switch (digitAsNumber)
-    {
-        case 0:
-            digitInWrittenForm = ZERO;
-            break;
-        case 1:
-            digitInWrittenForm = UN;
-            break;
-        case 2:
-            digitInWrittenForm = DEUX;
-            break;
-        case 3:
-            digitInWrittenForm = TROIS;
-            break;
-        case 4:
-            digitInWrittenForm = QUATRE;
-            break;
-        case 5:
-            digitInWrittenForm = CINQ;
-            break;
-        case 6:
-            digitInWrittenForm = SIX;
-            break;
-        case 7:
-            digitInWrittenForm = SEPT;
-            break;
-        case 8:
-            digitInWrittenForm = HUIT;
-            break;
-        case 9:
-            digitInWrittenForm = NEUF;
-            break;
-    }
-    return digitInWrittenForm;
+function getDigitInWrittenForm(digitAsNumber) {
+  let digitInWrittenForm = null;
+  if (
+    digitAsNumber === null ||
+    digitAsNumber === undefined ||
+    isNaN(digitAsNumber)
+  ) {
+    throw new Error("The passed argument is not a number");
+  } else if (digitAsNumber < 0 || digitAsNumber > 9) {
+    throw new Error(
+      "The pasased digit must be any one of the ten numbers 0 to 9"
+    );
+  }
+
+  switch (digitAsNumber) {
+    case 0:
+      digitInWrittenForm = ZERO;
+      break;
+    case 1:
+      digitInWrittenForm = UN;
+      break;
+    case 2:
+      digitInWrittenForm = DEUX;
+      break;
+    case 3:
+      digitInWrittenForm = TROIS;
+      break;
+    case 4:
+      digitInWrittenForm = QUATRE;
+      break;
+    case 5:
+      digitInWrittenForm = CINQ;
+      break;
+    case 6:
+      digitInWrittenForm = SIX;
+      break;
+    case 7:
+      digitInWrittenForm = SEPT;
+      break;
+    case 8:
+      digitInWrittenForm = HUIT;
+      break;
+    case 9:
+      digitInWrittenForm = NEUF;
+      break;
+  }
+  return digitInWrittenForm;
 }
 
 // Return the letter representation of a group. A group is made up of
 // three digits, like 123, 012, 001, 100, 101,...
-function getGroupInWrittenForm(groupAsNumber)
-{
-    if(groupAsNumber === null || groupAsNumber === undefined || isNaN(groupAsNumber)){
-        throw new Error("The passed argument is not a number");
-    }
-    let result = "";
+function getGroupInWrittenForm(groupAsNumber) {
+  if (
+    groupAsNumber === null ||
+    groupAsNumber === undefined ||
+    isNaN(groupAsNumber)
+  ) {
+    throw new Error("The passed argument is not a number");
+  }
+  let result = "";
 
-    if (groupAsNumber == 100){
-        result = CENT;
-        return result.trim();
-    }
-
-    if (groupAsNumber > 100)
-    {
-        // If the group value > 199,  then we placed ahead to count of hundreds
-        if (groupAsNumber / 100 > 1)
-            result = getDigitInWrittenForm(Math.floor(groupAsNumber / 100)) + " ";
-
-        result += CENT;
-        if ((groupAsNumber / 100 > 1) && (groupAsNumber % 100 == 0))
-            result += "s ";
-        else
-            result += " ";
-
-        groupAsNumber = groupAsNumber % 100;
-    }
-
-    let tensDigit = Math.floor(groupAsNumber / 10);
-    let onesDigit = groupAsNumber % 10;
-    switch (tensDigit)
-    {
-        case 0:
-            if (onesDigit != 0)
-                result += getDigitInWrittenForm(onesDigit);
-            break;
-        case 1:
-            if(onesDigit == 0)
-            {
-                result += DIX;
-            }
-            else if (onesDigit == 1)
-            {
-                result += ONZE;
-            }
-            else if(onesDigit == 2)
-            {
-                result += DOUZE;
-            }
-            else if(onesDigit == 3)
-            {
-                result += TREIZE;
-            }
-            else if(onesDigit == 4)
-            {
-                result += QUATORZE;
-            }
-            else if(onesDigit == 5)
-            {
-                result += QUINZE;
-            }
-            else if(onesDigit == 6)
-            {
-                result += SEIZE;
-            }
-            else
-            {
-                result += DIX + "-" + getDigitInWrittenForm(onesDigit);
-            }
-            break;
-        case 2:
-            result += VINGT;
-            if (onesDigit == 1)
-                result += " et";
-            if (onesDigit == 1)
-                result += " " + getDigitInWrittenForm(onesDigit);
-            if (onesDigit > 1)
-                result += "-" + getDigitInWrittenForm(onesDigit);
-            if (onesDigit == 0)
-                result += "s";
-            break;
-        case 3:
-            result += TRENTE;
-            if (onesDigit == 1)
-                result += " et";
-            if (onesDigit == 1)
-                result += " " + getDigitInWrittenForm(onesDigit);
-            if (onesDigit > 1)
-                result += "-" + getDigitInWrittenForm(onesDigit);
-            break;
-        case 4:
-            result += QUARANTE;
-            if (onesDigit == 1)
-                result += " et";
-            if (onesDigit == 1)
-                result += " " + getDigitInWrittenForm(onesDigit);
-            if (onesDigit > 1)
-                result += "-" + getDigitInWrittenForm(onesDigit);
-            break;
-        case 5:
-            result += CINQUANTE;
-            if (onesDigit == 1)
-                result += " et";
-            if (onesDigit == 1)
-                result += " " + getDigitInWrittenForm(onesDigit);
-            if (onesDigit > 1)
-                result += "-" + getDigitInWrittenForm(onesDigit);
-            break;
-        case 6:
-            result += SOIXANTE;
-            if (onesDigit == 1)
-                result += " et";
-            if (onesDigit == 1)
-                result += " " + getDigitInWrittenForm(onesDigit);
-            if (onesDigit > 1)
-                result += "-" + getDigitInWrittenForm(onesDigit);
-            break;
-        case 7:
-            result += SOIXANTE;
-            if (onesDigit == 1)
-                result += " et";
-
-            if (onesDigit == 0)
-            {
-                result += DIX;
-            }
-            else if (onesDigit == 1)
-            {
-                result += ONZE;
-            }
-            else if (onesDigit == 2)
-            {
-                result += DOUZE;
-            }
-            else if (onesDigit == 3)
-            {
-                result += TREIZE;
-            }
-            else if (onesDigit == 4)
-            {
-                result += QUATORZE;
-            }
-            else if (onesDigit == 5)
-            {
-                result += QUINZE;
-            }
-            else if (onesDigit == 6)
-            {
-                result += SEIZE;
-            }
-            else
-            {
-                result += DIX + "-" + getDigitInWrittenForm(onesDigit);
-            }
-            break;
-        case 8:
-            result += QUATRE + "-" + VINGT;
-            if (onesDigit > 0)
-                result += "-" + getDigitInWrittenForm(onesDigit);
-            if (onesDigit == 0)
-                result += "s";
-            break;
-        case 9:
-            result += QUATRE + "-" + VINGT + "-";
-                                if (onesDigit == 1)
-                result += " et";
-            if (onesDigit == 0)
-            {
-                result += DIX;
-            }
-            else if (onesDigit == 1)
-            {
-                result += ONZE;
-            }
-            else if (onesDigit == 2)
-            {
-                result += DOUZE;
-            }
-            else if (onesDigit == 3)
-            {
-                result += TREIZE;
-            }
-            else if (onesDigit == 4)
-            {
-                result += QUATORZE;
-            }
-            else if (onesDigit == 5)
-            {
-                result += QUINZE;
-            }
-            else if (onesDigit == 6)
-            {
-                result += SEIZE;
-            }
-            else
-            {
-                result += DIX + "-" + getDigitInWrittenForm(onesDigit);
-            }
-            break;
-    }
-
+  if (groupAsNumber == 100) {
+    result = CENT;
     return result.trim();
+  }
+
+  if (groupAsNumber > 100) {
+    // If the group value > 199,  then we placed ahead to count of hundreds
+    if (groupAsNumber / 100 > 1)
+      result = getDigitInWrittenForm(Math.floor(groupAsNumber / 100)) + " ";
+
+    result += CENT;
+    if (groupAsNumber / 100 > 1 && groupAsNumber % 100 == 0) result += "s ";
+    else result += " ";
+
+    groupAsNumber = groupAsNumber % 100;
+  }
+
+  let tensDigit = Math.floor(groupAsNumber / 10);
+  let onesDigit = groupAsNumber % 10;
+  switch (tensDigit) {
+    case 0:
+      if (onesDigit != 0) result += getDigitInWrittenForm(onesDigit);
+      break;
+    case 1:
+      if (onesDigit == 0) {
+        result += DIX;
+      } else if (onesDigit == 1) {
+        result += ONZE;
+      } else if (onesDigit == 2) {
+        result += DOUZE;
+      } else if (onesDigit == 3) {
+        result += TREIZE;
+      } else if (onesDigit == 4) {
+        result += QUATORZE;
+      } else if (onesDigit == 5) {
+        result += QUINZE;
+      } else if (onesDigit == 6) {
+        result += SEIZE;
+      } else {
+        result += DIX + "-" + getDigitInWrittenForm(onesDigit);
+      }
+      break;
+    case 2:
+      result += VINGT;
+      if (onesDigit == 1) result += " et";
+      if (onesDigit == 1) result += " " + getDigitInWrittenForm(onesDigit);
+      if (onesDigit > 1) result += "-" + getDigitInWrittenForm(onesDigit);
+      if (onesDigit == 0) result += "s";
+      break;
+    case 3:
+      result += TRENTE;
+      if (onesDigit == 1) result += " et";
+      if (onesDigit == 1) result += " " + getDigitInWrittenForm(onesDigit);
+      if (onesDigit > 1) result += "-" + getDigitInWrittenForm(onesDigit);
+      break;
+    case 4:
+      result += QUARANTE;
+      if (onesDigit == 1) result += " et";
+      if (onesDigit == 1) result += " " + getDigitInWrittenForm(onesDigit);
+      if (onesDigit > 1) result += "-" + getDigitInWrittenForm(onesDigit);
+      break;
+    case 5:
+      result += CINQUANTE;
+      if (onesDigit == 1) result += " et";
+      if (onesDigit == 1) result += " " + getDigitInWrittenForm(onesDigit);
+      if (onesDigit > 1) result += "-" + getDigitInWrittenForm(onesDigit);
+      break;
+    case 6:
+      result += SOIXANTE;
+      if (onesDigit == 1) result += " et";
+      if (onesDigit == 1) result += " " + getDigitInWrittenForm(onesDigit);
+      if (onesDigit > 1) result += "-" + getDigitInWrittenForm(onesDigit);
+      break;
+    case 7:
+      result += SOIXANTE;
+      if (onesDigit == 1) result += " et";
+
+      if (onesDigit == 0) {
+        result += DIX;
+      } else if (onesDigit == 1) {
+        result += ONZE;
+      } else if (onesDigit == 2) {
+        result += DOUZE;
+      } else if (onesDigit == 3) {
+        result += TREIZE;
+      } else if (onesDigit == 4) {
+        result += QUATORZE;
+      } else if (onesDigit == 5) {
+        result += QUINZE;
+      } else if (onesDigit == 6) {
+        result += SEIZE;
+      } else {
+        result += DIX + "-" + getDigitInWrittenForm(onesDigit);
+      }
+      break;
+    case 8:
+      result += QUATRE + "-" + VINGT;
+      if (onesDigit > 0) result += "-" + getDigitInWrittenForm(onesDigit);
+      if (onesDigit == 0) result += "s";
+      break;
+    case 9:
+      result += QUATRE + "-" + VINGT + "-";
+      if (onesDigit == 1) result += " et";
+      if (onesDigit == 0) {
+        result += DIX;
+      } else if (onesDigit == 1) {
+        result += ONZE;
+      } else if (onesDigit == 2) {
+        result += DOUZE;
+      } else if (onesDigit == 3) {
+        result += TREIZE;
+      } else if (onesDigit == 4) {
+        result += QUATORZE;
+      } else if (onesDigit == 5) {
+        result += QUINZE;
+      } else if (onesDigit == 6) {
+        result += SEIZE;
+      } else {
+        result += DIX + "-" + getDigitInWrittenForm(onesDigit);
+      }
+      break;
+  }
+
+  return result.trim();
 }
 
-export function NombresEnToutesLettres(){
-    return "Hello world!";
+function convert(numberAsString) {
+  let p1 = numberAsString.indexOf(".");
+  let p2 = numberAsString.indexOf(",");
+
+  // float
+  if (p1 != -1) {
+    let IntegerPart = numberAsString.substring(0, p1);
+    let decimalPart = numberAsString.substring(p1 + 1);
+    numberAsString =
+      getLetters(IntegerPart) + " virgule " + getLetters(decimalPart);
+  }
+  if (p2 != -1) {
+    let IntegerPart = numberAsString.substring(0, p2);
+    let decimalPart = numberAsString.substring(p2 + 1);
+    numberAsString =
+      getLetters(IntegerPart) + " virgule " + getLetters(decimalPart);
+  }
+
+  // integer
+  if (p1 == -1 && p2 == -1) {
+    return getLetters(numberAsString);
+  }
+
+  return numberAsString;
 }
 
+/**
+ * Returns the passed number in a written form
+ *
+ * For instance: 1234567890 becomes: un milliard deux cent trente quatre million cinq
+ * cent soixante sept mille huit cent quatre vingt dix
+ */
+function getLetters(number) {
+  if (number === null || number === undefined || isNaN(number)) {
+    throw new Error("The passed argument is not a number");
+  }
+  number = parseInt(number);
+  if (number === 0) return ZERO;
+
+  let polaritySign = "";
+  if (number < 0) {
+    number = -number;
+    polaritySign = MOINS + " ";
+  }
+  let result = "";
+  let rang = 0;
+  while (number > 0) {
+    let plural = "";
+    if (tab[rang] === "million" || tab[rang] === "milliard") {
+      if (number % 1000 > 1) plural = "s";
+    }
+
+    // prepend result with the next group in written formn
+    if (number % 1000 !== 0)
+      result =
+        getGroupInWrittenForm(number % 1000) +
+        " " +
+        tab[rang] +
+        plural +
+        " " +
+        result;
+
+    if (tab[rang] === "mille" && number % 1000 === 1) {
+      result = result.substring(3); // removes one "un "
+    }
+
+    // move to the next group
+    number = Math.floor(number / 1000);
+    rang++;
+  }
+
+  result = polaritySign + result;
+  return result.trim();
+}
+
+export function NombresEnToutesLettres() {
+  return "Hello world!";
+}
 
 export const exportedForTesting = {
-    getDigitInWrittenForm,
-    getGroupInWrittenForm
-}
+  getDigitInWrittenForm,
+  getGroupInWrittenForm,
+  getLetters,
+  convert,
+};
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,38 +1,75 @@
-'use strict';
+"use strict";
 
-import { NombresEnToutesLettres, exportedForTesting } from '../src/index';
-const { getDigitInWrittenForm, getGroupInWrittenForm } = exportedForTesting;
+import { NombresEnToutesLettres, exportedForTesting } from "../src/index";
+const { getDigitInWrittenForm, getGroupInWrittenForm, getLetters, convert } =
+  exportedForTesting;
 
 describe("NombreEnToutesLettres class", () => {
-    it("returns the written form of a digit", () =>{
-        expect(getDigitInWrittenForm(0)).toBe("zéro");
-        expect(getDigitInWrittenForm(1)).toBe("un");
-        expect(getDigitInWrittenForm(9)).toBe("neuf");
+  it("returns the written form of a digit", () => {
+    expect(getDigitInWrittenForm(0)).toBe("zéro");
+    expect(getDigitInWrittenForm(1)).toBe("un");
+    expect(getDigitInWrittenForm(9)).toBe("neuf");
 
-        expect(() => getDigitInWrittenForm(34)).toThrow();
-        expect(() => getDigitInWrittenForm(null)).toThrow();
-        expect(() => getDigitInWrittenForm("hdfkdkd")).toThrow();
-        expect(() => getDigitInWrittenForm(undefined)).toThrow();
-        expect(() => getDigitInWrittenForm({})).toThrow();
-    });
+    expect(() => getDigitInWrittenForm(34)).toThrow();
+    expect(() => getDigitInWrittenForm(null)).toThrow();
+    expect(() => getDigitInWrittenForm("hdfkdkd")).toThrow();
+    expect(() => getDigitInWrittenForm(undefined)).toThrow();
+    expect(() => getDigitInWrittenForm({})).toThrow();
+  });
 
-    it("returns the written form of a group such as 324, 102, 999... ", () =>{
-        expect(getGroupInWrittenForm(100)).toBe("cent");
-        expect(getGroupInWrittenForm(325)).toBe("trois cent vingt-cinq");
-        expect(getGroupInWrittenForm(200)).toBe("deux cents");
-        expect(getGroupInWrittenForm(999)).toBe("neuf cent quatre-vingt-dix-neuf");
-        expect(getGroupInWrittenForm(801)).toBe("huit cent un");
-        expect(getGroupInWrittenForm(2)).toBe("deux");
+  it("returns the written form of a group such as 324, 102, 999... ", () => {
+    expect(getGroupInWrittenForm(100)).toBe("cent");
+    expect(getGroupInWrittenForm(325)).toBe("trois cent vingt-cinq");
+    expect(getGroupInWrittenForm(200)).toBe("deux cents");
+    expect(getGroupInWrittenForm(999)).toBe("neuf cent quatre-vingt-dix-neuf");
+    expect(getGroupInWrittenForm(801)).toBe("huit cent un");
+    expect(getGroupInWrittenForm(2)).toBe("deux");
 
-        expect(() => getGroupInWrittenForm(null)).toThrow();
-        expect(() => getGroupInWrittenForm("hdfkdkd")).toThrow();
-        expect(() => getGroupInWrittenForm(undefined)).toThrow();
-        expect(() => getGroupInWrittenForm({})).toThrow();
-    });
+    expect(() => getGroupInWrittenForm(null)).toThrow();
+    expect(() => getGroupInWrittenForm("hdfkdkd")).toThrow();
+    expect(() => getGroupInWrittenForm(undefined)).toThrow();
+    expect(() => getGroupInWrittenForm({})).toThrow();
+  });
 
-    it('returns "Hello world!"', () => {
-        expect(NombresEnToutesLettres()).toBe("Hello world!");
-      });
-})
+  it("returns the written form of a number such as 203,3422 or 8664.134 ", () => {
+    expect(convert("10345,453")).toBe(
+      "dix mille trois cent quarante-cinq virgule quatre cent cinquante-trois"
+    );
+    expect(convert("203325.01")).toBe(
+      "deux cent trois mille trois cent vingt-cinq virgule un"
+    );
+    expect(convert("403")).toBe("quatre cent trois");
+    expect(convert("1")).toBe("un");
+    expect(convert("0")).toBe("zéro");
+    expect(convert("29999999,75632")).toBe(
+      "vingt-neuf millions neuf cent quatre-vingt-dix-neuf mille neuf cent quatre-vingt-dix-neuf virgule soixantequinze mille six cent trente-deux"
+    );
 
+    expect(() => getLetters(null)).toThrow();
+    expect(() => getLetters("hdfkdkd")).toThrow();
+    expect(() => getLetters(undefined)).toThrow();
+    expect(() => getLetters({})).toThrow();
+  });
 
+  it("returns the written form of a number such as 203953 or 287 ", () => {
+    expect(getLetters(10345)).toBe("dix mille trois cent quarante-cinq");
+    expect(getLetters(203325)).toBe(
+      "deux cent trois mille trois cent vingt-cinq"
+    );
+    expect(getLetters(200)).toBe("deux cents");
+    expect(getLetters(1)).toBe("un");
+    expect(getLetters(0)).toBe("zéro");
+    expect(getLetters(29999999)).toBe(
+      "vingt-neuf millions neuf cent quatre-vingt-dix-neuf mille neuf cent quatre-vingt-dix-neuf"
+    );
+
+    expect(() => getLetters(null)).toThrow();
+    expect(() => getLetters("hdfkdkd")).toThrow();
+    expect(() => getLetters(undefined)).toThrow();
+    expect(() => getLetters({})).toThrow();
+  });
+
+  it('returns "Hello world!"', () => {
+    expect(NombresEnToutesLettres()).toBe("Hello world!");
+  });
+});
